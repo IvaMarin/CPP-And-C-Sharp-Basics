64fd4aea1c515f7e0af798a30c43a073253a65f0
IvaMarin <iamarinichev@gmail.com>
2023-10-12 00:28:47 +0300

    finished 2d array task

--- lesson.cpp
+++ lesson.cpp
@@ -2,7 +2,35 @@
 
 int main(const int argc, const char *argv[])
 {
-    std::cout << "Hello world!\n";
-    // some changes here
+    int N = 0;
+    int m = 0;
+    int k = 0;
+    std::cin >> N >> m >> k;
+
+    int **array = (int **)calloc(sizeof(int *), k);
+    for (int i = 0; i < k; i++)
+    {
+        array[i] = (int *)calloc(sizeof(int), m);
+    }
+    
+    int counter = 0;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            array[i][j] = N + counter;
+            counter++;
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            std::cout << array[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
