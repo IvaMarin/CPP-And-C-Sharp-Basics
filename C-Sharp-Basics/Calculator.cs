6ad0df56a5398121bcdc91fca9060aba97102e98
Vanya <iamarinichev@gmail.com>
2023-11-14 23:26:22 +0300

    finished basic calculator task

---
+++ C-Sharp-Basics/Calculator.cs
@@ -0,0 +1,37 @@
+﻿namespace C_Sharp_Basics
+{
+    internal class Calculator
+    {
+        public static decimal Add(decimal firstNumber, decimal secondNumber)
+        {
+            decimal sum = firstNumber + secondNumber;
+            return sum;
+        }
+
+        public static decimal Subtract(decimal firstNumber, decimal secondNumber)
+        {
+            decimal difference = firstNumber - secondNumber;
+            return difference;
+        }
+
+        public static decimal Divide(decimal firstNumber, decimal secondNumber)
+        {
+            if (secondNumber != 0)
+            {
+                decimal quotient = firstNumber / secondNumber;
+                return quotient;
+            }
+            else
+            {
+                throw new ArgumentException("can't divide by zero");
+            }
+        }
+
+        public static decimal Multiply(decimal firstNumber, decimal secondNumber)
+        {
+            decimal product = firstNumber * secondNumber;
+            return product;
+        }
+    }
+}
+
---
+++ C-Sharp-Basics/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace C_Sharp_Basics
+{
+    internal class Program
+    {
+        public static decimal TryReadNumber()
+        {
+            string? numberString;
+            decimal number;
+            while (true)
+            {
+
+                numberString = Console.ReadLine();
+
+                if (decimal.TryParse(numberString, out decimal result))
+                {
+                    number = result;
+                    break;
+                }
+                else
+                {
+                    Console.Write("Enter correct number: ");
+                }
+            }
+            return number;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            decimal firstNumber = TryReadNumber();
+
+            Console.Write("Enter second number: ");
+            decimal secondNumber = TryReadNumber();
+
+            Console.WriteLine($"Sum: {Calculator.Add(firstNumber, secondNumber)}");
+            Console.WriteLine($"Difference: {Calculator.Subtract(firstNumber, secondNumber)}");
+            Console.WriteLine($"Product: {Calculator.Multiply(firstNumber, secondNumber)}");
+            if (secondNumber != 0)
+            {
+                Console.WriteLine($"Quotient: {Calculator.Divide(firstNumber, secondNumber)}");
+            }
+            else
+            {
+                Console.WriteLine("Can't divide by zero");
+            }
+
+        }
+    }
+}
